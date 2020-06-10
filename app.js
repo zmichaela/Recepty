a2dda1181a0e09ddd69b307b389a9db9219565af
zmichaela <m.zajacova90@gmail.com>
2020-06-10 17:28:11 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+/*
+Co budeme dělat:
+
+1) Do prvku s id="recepty" vygenerujeme z dat seznam všech receptů z naší "databáze".
+HTML vzor, jak vygenerovaný recept vypadá, je zakomentovaný v index.html.
+
+2) Doplň hledání - při kliknutí na tlačítko Hledat
+by se měl seznam receptů vyfiltrovat podle hledaného slova.
+
+3) Doplň filtrovanání receptů podle kategorie.
+
+4) Doplň řazení receptů podle hodnocení.
+
+5) DOMÁCÍ ÚKOL:
+Na recepty v seznamu by mělo jít kliknout a na pravé polovině, se objeví detail receptu.
+Doplň patričné údaje receptu do HTML prvků s ID recept-foto, recept-kategorie,
+recept-hodnoceni, recept-nazev, recept-popis.
+*/
+
+const elRecepty = document.querySelector("#recepty");
+
+vypisSeznamReceptu(){
+    let obsah = "";
+    elRecepty
+};
+
+
